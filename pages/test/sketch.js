3c5a4787483176ae4312534fd1a329386b2be66d
wiroon.sri@WSYMBP16M2 <wiroon.sri@innosoft.kmutt.ac.th>
2024-02-14 10:13:52 +0700

    add test page

---
+++ pages/test/sketch.js
@@ -0,0 +1,12 @@
+console.log('start')
+
+function setup() {
+  console.log('setup');
+  // put setup code here
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  // put drawing code here
+}
